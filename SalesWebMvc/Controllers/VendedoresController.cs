b353c6948b5b070b798e010f4f9b772128705d04
Weslley Pontes <contato.weslleypontes@gmail.com>
2022-04-16 11:34:58 -0300

    VendedoresController

---
+++ SalesWebMvc/Controllers/VendedoresController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SalesWebMvc.Controllers
+{
+    public class VendedoresController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
